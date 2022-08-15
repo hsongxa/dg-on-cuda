18f8c28d1032562d19b2b9e03ebcfdd6b43ddd22
Hao Song <hsongxa@gmail.com>
2022-08-14 20:41:56 -0600

    discrete operators of divergence and gradient

--- examples/stokes_2d/stokes_2d.h
+++ examples/stokes_2d/stokes_2d.h
@@ -27,10 +27,11 @@
 
 #include <utility>
 #include <vector>
-#include <math.h>
+#include <cmath>
 
 #include <thrust/tuple.h>
 
+#include "laplace_2d.h"
 #include "simple_discretization_2d.h"
 
 // host code that represent the problem of Stokes equation in two dimensional space
@@ -56,12 +57,6 @@ public:
   template<typename ConstZipItr, typename ZipItr>
   void advance_timestep(ConstZipItr in0, ConstZipItr in1, std::size_t size, T t, T dt, ZipItr out) const;
 
-  // need to easily copy these data to device so make them public
-  using typename dgc::simple_discretization_2d<T, M>::dense_matrix_t;
-  dense_matrix_t m_Dr;
-  dense_matrix_t m_Ds;
-  dense_matrix_t m_L;
-
 private:
   using typename dgc::simple_discretization_2d<T, M>::reference_element;
   using typename dgc::simple_discretization_2d<T, M>::mapping;
@@ -69,61 +64,37 @@ private:
   // (m_u_tilde_x, m_u_tilde_y) => m_p
   void solve_pressure() const;
 
-  template<typename ConstZipItr>
-  void numerical_fluxes(ConstZipItr input, T t) const; // time t is used for boundary conditions
+  dgc::laplace_2d<T, M> m_laplace_2d;
 
-  // work space for numerical fluxes to avoid repeated allocations
-  mutable std::vector<T> m_numericalFlux_Ux;
-  mutable std::vector<T> m_numericalFlux_Uy;
-  mutable std::vector<T> m_numericalFlux_P;
+  // work space
   mutable std::vector<T> m_u_tilde_x;
   mutable std::vector<T> m_u_tilde_y;
+  mutable std::vector<T> m_div_u;
   mutable std::vector<T> m_p;
 
   static constexpr T a = 2.883356L;
   static constexpr T lamda = dgc::const_val<T, 1> + a * a;
+
+private:
+  struct pressure_bc
+  {
+    // Table 1 of the 2017 paper by N. Fehn et al.
+    T exterior_val(T x, T y, T interior_val) { return - interior_val; }
+    T exterior_grad_n(T x, T y, T interior_grad_n) { return interior_grad_n; }
+  };
 };
 
 template<typename T, typename M>
 stokes_2d<T, M>::stokes_2d(const M& mesh, int order)
-  : dgc::simple_discretization_2d<T, M>(mesh, order)
+  : dgc::simple_discretization_2d<T, M>(mesh, order), m_laplace_2d(mesh, order)
 {
-  // volume integration matrices
-  reference_element refElem;
-  dense_matrix_t v = refElem.vandermonde_matrix(this->m_order);
-  dense_matrix_t mInv = v * v.transpose();
-  dense_matrix_t m = mInv.inverse();
-
-  auto vGrad = refElem.grad_vandermonde_matrix(this->m_order);
-  m_Dr = v * vGrad.first.transpose() * m; // this is the Dr in weak form
-  m_Ds = v * vGrad.second.transpose() * m; // this is the Ds in weak form
-
-  // surface integration matrix for triangle element
-  auto numFaceNodes = refElem.num_face_nodes(this->m_order);
-  auto numCellNodes = refElem.num_nodes(this->m_order);
-  dense_matrix_t mE(numCellNodes, 3 * numFaceNodes, dgc::const_val<T, 0>);
-  for (int e = 0; e < 3; ++e)
-  {
-    const std::vector<int>& eN = e == 0 ? this->F0_Nodes : (e == 1 ? this->F1_Nodes : this->F2_Nodes);
-
-    dense_matrix_t eV = refElem.face_vandermonde_matrix(this->m_order, e);
-    dense_matrix_t eM = eV * eV.transpose();
-    eM = eM.inverse();
-    for (std::size_t row = 0; row < eM.size_row(); ++row)
-      for (std::size_t col = 0; col < eM.size_col(); ++col)
-        mE(eN[row], e * numFaceNodes + col) = eM(row, col);
-  }
-  m_L = mInv * mE;
-
   // space allocation for intermediate variables
   auto numCells = this->m_mesh->num_cells();
+  auto numCellNodes = reference_element().num_nodes(this->m_order);
   m_u_tilde_x.resize(numCells * numCellNodes);
   m_u_tilde_y.resize(numCells * numCellNodes);
+  m_div_u.resize(numCells * numCellNodes);
   m_p.resize(numCells * numCellNodes);
-
-  m_numericalFlux_Ux.resize(numCells * 3 * numFaceNodes);
-  m_numericalFlux_Uy.resize(numCells * 3 * numFaceNodes);
-  m_numericalFlux_P.resize(numCells * 3 * numFaceNodes);
 }
 
 template<typename T, typename M> template<typename PosItr, typename DofItr>
@@ -132,9 +103,8 @@ void stokes_2d<T, M>::initialize_dofs(PosItr posItr, DofItr dofItr) const
   using point_type = dgc::point_2d<T>;
   using cell_type = typename M::cell_type;
 
-  reference_element refElem;
   std::vector<std::pair<T, T>> pos;
-  refElem.node_positions(this->m_order, std::back_inserter(pos));
+  reference_element().node_positions(this->m_order, std::back_inserter(pos));
 
   auto itTuple = dofItr.get_iterator_tuple();
   auto itUx = thrust::get<0>(itTuple);
@@ -162,9 +132,8 @@ void stokes_2d<T, M>::exact_solution(T t, ZipItr it) const
   using point_type = dgc::point_2d<T>;
   using cell_type = typename M::cell_type;
 
-  reference_element refElem;
   std::vector<std::pair<T, T>> pos;
-  refElem.node_positions(this->m_order, std::back_inserter(pos));
+  reference_element().node_positions(this->m_order, std::back_inserter(pos));
 
   auto itTuple = it.get_iterator_tuple();
   auto itUx = thrust::get<0>(itTuple);
@@ -187,89 +156,10 @@ void stokes_2d<T, M>::exact_solution(T t, ZipItr it) const
     }
 }
 
-template<typename T, typename M> template<typename ConstZipItr>
-void stokes_2d<T, M>::numerical_fluxes(ConstZipItr input, T t) const
-{
-  using point_type = dgc::point_2d<T>;
-  using cell_type = typename M::cell_type;
-
-  reference_element refElem;
-  std::vector<std::pair<T, T>> pos;
-  refElem.node_positions(this->m_order, std::back_inserter(pos));
-
-  int numCellNodes = refElem.num_nodes(this->m_order);
-  int numFaceNodes = refElem.num_face_nodes(this->m_order);
-  T aHx, bHx, aHy, bHy, aEz, bEz; // "a" is interior or "-" and "b" is exterior or "+"
-  T dltHx, dltHy, sumEz, nx, ny;
-
-  // one flux is computed twice from two different cells, rather than for each PAIR of half-edges
-  for (int c = 0; c < this->m_mesh->num_cells(); ++c)
-  {
-    const cell_type cell = this->m_mesh->get_cell(c);
-    for (int e = 0; e < 3; ++e)
-    {
-      bool isBoundary;
-      int nbCell, nbLocalEdgeIdx;
-      std::tie(isBoundary, nbCell, nbLocalEdgeIdx) = this->m_mesh->get_face_neighbor(c, e);
-
-      const std::vector<int>& faceNodes = e == 0 ? this->F0_Nodes : (e == 1 ? this->F1_Nodes : this->F2_Nodes);
-      for (int d = 0; d < numFaceNodes; ++d)
-      {
-        int inIdxA = c * numCellNodes + faceNodes[d]; // d.o.f. on this edge itself
-
-        // fetch the value on interior
-        aHx = thrust::get<0>(input[inIdxA]);
-        aHy = thrust::get<1>(input[inIdxA]);
-        aEz = thrust::get<2>(input[inIdxA]);
-
-        if(isBoundary)
-        {
-          bHx = aHx;
-          bHy = aHy;
-          bEz = - aEz;
-        }
-        else
-        {
-          const std::vector<int>& nbFaceNodes = nbLocalEdgeIdx == 0 ? this->F0_Nodes : (nbLocalEdgeIdx == 1 ? this->F1_Nodes : this->F2_Nodes);
-
-          // flip direction to match neighbor's edge d.o.f.'s with this edge's !
-          // THIS ONLY WORKS FOR 2D--FOR 3D WE MAY NEED TO GEOMETRICALLY MATCH D.O.F.'S !
-          // TODO: spot for future generalization and abstraction
-          int inIdxB = nbCell * numCellNodes + nbFaceNodes[numFaceNodes - d - 1];
-          assert(inIdxA != inIdxB);
-
-          bHx = thrust::get<0>(input[inIdxB]);
-          bHy = thrust::get<1>(input[inIdxB]);
-          bEz = thrust::get<2>(input[inIdxB]);
-
-          // extra verification that A and B are geometrically the same point
-          // can be commented out wihtout affecting calculations
-          point_type A = mapping::rs_to_xy(cell, point_type(pos[faceNodes[d]].first, pos[faceNodes[d]].second));
-          point_type B = mapping::rs_to_xy(this->m_mesh->get_cell(nbCell),
-                                           point_type(pos[nbFaceNodes[numFaceNodes - d - 1]].first,
-                                                      pos[nbFaceNodes[numFaceNodes - d - 1]].second));
-          assert(std::abs(A.x() - B.x()) < 1.0e-10);
-          assert(std::abs(A.y() - B.y()) < 1.0e-10);
-        }
-        
-        int outIdx = c * 3 * numFaceNodes + e * numFaceNodes + d;
-        point_type n = cell.outward_normal(e);
-        nx = n.x();
-        ny = n.y();
-        dltHx = aHx - bHx;
-        dltHy = aHy - bHy;
-        sumEz = aEz + bEz;
-        m_numericalFlux_Ux[outIdx] = (T)(0.5L) * ny * (sumEz + ny * dltHx - nx * dltHy);
-        m_numericalFlux_Uy[outIdx] = - (T)(0.5L) * nx * (sumEz + ny * dltHx - nx * dltHy);
-        m_numericalFlux_P[outIdx] = (T)(0.5L) * (ny * (aHx + bHx) - nx * (aHy + bHy) + aEz - bEz);
-      }
-    }
-  }
-}
-
 template<typename T, typename M>
 void stokes_2d<T, M>::solve_pressure() const
 {
+  m_laplace_2d(m_div_u, pressure_bc());
 }
 
 // t => t + dt
@@ -291,46 +181,6 @@ void stokes_2d<T, M>::advance_timestep(ConstZipItr in0, ConstZipItr in1, std::si
 
   // stage 2: pressure step
   solve_pressure();
-/*
-  reference_element refElem;
-  int numCellNodes = refElem.num_nodes(this->m_order);
-  int numFaceNodes = refElem.num_face_nodes(this->m_order);
-
-  // surface-mapped numerical fluxes
-  std::vector<T> fHx(3 * numFaceNodes), fHy(3 * numFaceNodes), fEz(3 * numFaceNodes);
-
-
-  for (int c = 0; c < this->m_mesh->num_cells(); ++c)
-  {
-    dense_matrix_t Dx = m_Dr * this->Inv_Jacobians[c * 4] + m_Ds * this->Inv_Jacobians[c * 4 + 2];
-    dense_matrix_t Dy = m_Dr * this->Inv_Jacobians[c * 4 + 1] + m_Ds * this->Inv_Jacobians[c * 4 + 3];
-
-    int offsetC = c * numCellNodes;
-    Dy.gemv(dgc::const_val<T, 1>, inEz + offsetC, dgc::const_val<T, 0>, outHx + offsetC);
-    Dx.gemv(- dgc::const_val<T, 1>, inEz + offsetC, dgc::const_val<T, 0>, outHy + offsetC);
-    Dx.gemv(- dgc::const_val<T, 1>, inHy + offsetC, dgc::const_val<T, 0>, outEz + offsetC);
-    Dy.gemv(dgc::const_val<T, 1>, inHx + offsetC, dgc::const_val<T, 1>, outEz + offsetC);
-
-    // fetch numerical fluxes and apply face mapping
-    const auto cell = this->m_mesh->get_cell(c);
-    int offsetF = c * 3 * numFaceNodes;
-    for (int e = 0; e < 3; ++e)
-    {
-      T faceJ = mapping::face_J(cell, e);
-      for (int i = 0; i < numFaceNodes; ++i)
-      {
-        int ei = e * numFaceNodes + i;
-        fHx[ei] = m_numericalFlux_Ux[offsetF + ei] * faceJ;
-        fHy[ei] = m_numericalFlux_Uy[offsetF + ei] * faceJ;
-        fEz[ei] = m_numericalFlux_P[offsetF + ei] * faceJ;
-      }
-    }
-
-    T cellJ = mapping::J(cell);
-    m_L.gemv(- dgc::const_val<T, 1> / cellJ, fHx.cbegin(), dgc::const_val<T, 1>, outHx + offsetC);
-    m_L.gemv(- dgc::const_val<T, 1> / cellJ, fHy.cbegin(), dgc::const_val<T, 1>, outHy + offsetC);
-    m_L.gemv(- dgc::const_val<T, 1> / cellJ, fEz.cbegin(), dgc::const_val<T, 1>, outEz + offsetC);
-  }
-*/}
+}
 
 #endif
---
+++ src/div_2d.h
@@ -0,0 +1,192 @@
+/**
+ * MIT License
+ * 
+ * Copyright (c) 2021 hsongxa
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ **/
+
+#ifndef DIV_2D_H
+#define DIV_2D_H 
+
+#include <algorithm>
+#include <utility>
+
+#include "simple_discretization_2d.h"
+
+BEGIN_NAMESPACE
+
+// discrete divergence operator with central flux in two dimensional space
+// using simple discretization
+template<typename T, typename M> // T - number type, M - mesh type
+class div_2d : public simple_discretization_2d<T, M>
+{
+public:
+  div_2d(const M& mesh, int order); 
+  ~div_2d(){}
+  
+  // operate in place - the result is stored in in0
+  template<typename RandItr, typename BC2D>
+  void operator()(RandItr in0, RandItr in1, std::size_t size, BC2D bc) const;
+
+private:
+  using typename simple_discretization_2d<T, M>::reference_element;
+  using typename simple_discretization_2d<T, M>::mapping;
+  using typename simple_discretization_2d<T, M>::dense_matrix_t;
+  dense_matrix_t m_Dr;
+  dense_matrix_t m_Ds;
+  dense_matrix_t m_L;
+
+  template<typename RandItr, typename BC2D>
+  void numerical_fluxes(RandItr in0, RandItr in1, BC2D bc) const;
+  mutable std::vector<T> m_numericalFluxes;
+};
+
+template<typename T, typename M>
+div_2d<T, M>::div_2d(const M& mesh, int order) : simple_discretization_2d<T, M>(mesh, order)
+{
+  // volume integration matrices
+  reference_element refElem;
+  dense_matrix_t v = refElem.vandermonde_matrix(this->m_order);
+  dense_matrix_t mInv = v * v.transpose();
+  dense_matrix_t m = mInv.inverse();
+
+  auto vGrad = refElem.grad_vandermonde_matrix(this->m_order);
+  m_Dr = v * vGrad.first.transpose() * m; // this is the Dr in weak form
+  m_Ds = v * vGrad.second.transpose() * m; // this is the Ds in weak form
+
+  // surface integration matrix for triangle element
+  auto numFaceNodes = refElem.num_face_nodes(this->m_order);
+  dense_matrix_t mE(refElem.num_nodes(this->m_order), 3 * numFaceNodes, const_val<T, 0>);
+  for (int e = 0; e < 3; ++e)
+  {
+    const std::vector<int>& eN = e == 0 ? this->F0_Nodes : (e == 1 ? this->F1_Nodes : this->F2_Nodes);
+
+    dense_matrix_t eV = refElem.face_vandermonde_matrix(this->m_order, e);
+    dense_matrix_t eM = eV * eV.transpose();
+    eM = eM.inverse();
+    for (std::size_t row = 0; row < eM.size_row(); ++row)
+      for (std::size_t col = 0; col < eM.size_col(); ++col)
+        mE(eN[row], e * numFaceNodes + col) = eM(row, col);
+  }
+  m_L = mInv * mE;
+
+  // space allocation for numerical fluxes
+  m_numericalFluxes.resize(this->m_mesh->num_cells() * 3 * numFaceNodes);
+}
+
+template<typename T, typename M> template<typename RandItr, typename BC2D>
+void div_2d<T, M>::numerical_fluxes(RandItr in0, RandItr in1, BC2D bc) const
+{
+  using point_type = point_2d<T>;
+  using cell_type = typename M::cell_type;
+
+  reference_element refElem;
+  std::vector<std::pair<T, T>> pos;
+  refElem.node_positions(this->m_order, std::back_inserter(pos));
+
+  int numCellNodes = refElem.num_nodes(this->m_order);
+  int numFaceNodes = refElem.num_face_nodes(this->m_order);
+  T aU0, aU1, bU0, bU1; // "a" is interior or "-" and "b" is exterior or "+"
+
+  // one flux is computed twice from two different cells, rather than for each PAIR of half-edges
+  for (int c = 0; c < this->m_mesh->num_cells(); ++c)
+  {
+    const cell_type cell = this->m_mesh->get_cell(c);
+    for (int e = 0; e < 3; ++e)
+    {
+      bool isBoundary;
+      int nbCell, nbLocalEdgeIdx;
+      std::tie(isBoundary, nbCell, nbLocalEdgeIdx) = this->m_mesh->get_face_neighbor(c, e);
+
+      const std::vector<int>& faceNodes = e == 0 ? this->F0_Nodes : (e == 1 ? this->F1_Nodes : this->F2_Nodes);
+      for (int d = 0; d < numFaceNodes; ++d)
+      {
+        int inIdxA = c * numCellNodes + faceNodes[d]; // d.o.f. on this edge itself
+        aU0 = *(in0 + inIdxA);
+        aU1 = *(in1 + inIdxA);
+
+        if(isBoundary)
+        {
+          point_type xyPos = mapping::rs_to_xy(cell, point_type(pos[faceNodes[d]].first, pos[faceNodes[d]].second));
+          bU0 = bc.exterior_val(xyPos.x(), xyPos.y(), aU0);
+          bU1 = bc.exterior_val(xyPos.x(), xyPos.y(), aU1);
+        }
+        else
+        {
+          const std::vector<int>& nbFaceNodes = nbLocalEdgeIdx == 0 ? this->F0_Nodes : (nbLocalEdgeIdx == 1 ? this->F1_Nodes : this->F2_Nodes);
+
+          // flip direction to match neighbor's edge d.o.f.'s with this edge's !
+          // THIS ONLY WORKS FOR 2D--FOR 3D WE MAY NEED TO GEOMETRICALLY MATCH D.O.F.'S !
+          int inIdxB = nbCell * numCellNodes + nbFaceNodes[numFaceNodes - d - 1];
+          assert(inIdxA != inIdxB);
+          bU0 = *(in0 + inIdxB);
+          bU1 = *(in1 + inIdxB);
+        }
+        
+        // central flux
+        point_type n = cell.outward_normal(e);
+        m_numericalFluxes[c * 3 * numFaceNodes + e * numFaceNodes + d]
+          = ((aU0 + bU0) * n.x() + (aU1 + bU1) * n.y()) / const_val<T, 2>;
+      }
+    }
+  }
+}
+
+template<typename T, typename M> template<typename RandItr, typename BC2D>
+void div_2d<T, M>::operator()(RandItr in0, RandItr in1, std::size_t size, BC2D bc) const
+{
+  numerical_fluxes(in0, in1, bc);
+
+  reference_element refElem;
+  int numCellNodes = refElem.num_nodes(this->m_order);
+  int numFaceNodes = refElem.num_face_nodes(this->m_order);
+
+  std::vector<T> mFl(3 * numFaceNodes); // surface-mapped numerical fluxes
+  std::vector<T> cpy(numCellNodes); // cache of in0 for one cell at a time
+
+  for (int c = 0; c < this->m_mesh->num_cells(); ++c)
+  {
+    dense_matrix_t Dx = m_Dr * this->Inv_Jacobians[c * 4] + m_Ds * this->Inv_Jacobians[c * 4 + 2];
+    dense_matrix_t Dy = m_Dr * this->Inv_Jacobians[c * 4 + 1] + m_Ds * this->Inv_Jacobians[c * 4 + 3];
+
+    int offset = c * numCellNodes;
+    std::copy(in0 + offset, in0 + offset + numCellNodes, cpy.begin());
+    Dx.gemv(- const_val<T, 1>, cpy.cbegin(), const_val<T, 0>, in0 + offset);
+    Dy.gemv(- const_val<T, 1>, in1 + offset, const_val<T, 1>, in0 + offset);
+
+    // fetch numerical fluxes and apply face mapping
+    const auto cell = this->m_mesh->get_cell(c);
+    for (int e = 0; e < 3; ++e)
+    {
+      T faceJ = mapping::face_J(cell, e);
+      for (int i = 0; i < numFaceNodes; ++i)
+      {
+        int ei = e * numFaceNodes + i;
+        mFl[ei] = m_numericalFluxes[c * 3 * numFaceNodes + ei] * faceJ;
+      }
+    }
+
+    m_L.gemv(const_val<T, 1> / mapping::J(cell), mFl.cbegin(), const_val<T, 1>, in0 + offset);
+  }
+}
+
+END_NAMESPACE
+
+#endif
